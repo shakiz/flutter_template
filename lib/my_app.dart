6e1f666945d841c5b7d3956713b332a05793ae62
shakiz <activities335@gmail.com>
2024-02-11 12:58:03 +0600

    Update main app with provider dependencies

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/presentation/home/home_page.dart';
+import 'package:flutter_template/presentation/home/home_viewmodel.dart';
 import 'package:flutter_template/routing/app_pages.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      getPages: AppPages.routes,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => HomeViewModel())
+      ],
+      child: GetMaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        getPages: AppPages.routes,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
